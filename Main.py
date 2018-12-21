3b2028e9779c97bd350dfe8a854d7ddf93e7069e
szymonjuraszek <szymonjuraszek777@gmail.com>
2018-12-21 20:11:50 +0100

    removed column Education, and classification data

--- Main.py
+++ Main.py
@@ -21,9 +21,7 @@ original_data = pd.read_csv(
         sep=',',
         engine='python',
         na_values="?")
-
 print(original_data.head())
-
 
 # Histogram dla poszczegolnych kolumn
 fig = plt.figure(figsize=(20,15))
@@ -37,5 +35,39 @@ for i, column in enumerate(original_data.columns):
     else:
         original_data[column].hist(axes=ax)
         plt.xticks(rotation="vertical")
+plt.subplots_adjust(hspace=0.7, wspace=0.2)
+plt.show()
+
+# print((original_data["Country"].value_counts() / original_data.shape[0]).head());
+
+# Encode the categorical features as numbers
+def number_encode_features(df):
+    result = df.copy()
+    encoders = {}
+    for column in result.columns:
+        if result.dtypes[column] == np.object:
+            encoders[column] = preprocessing.LabelEncoder()
+            result[column] = encoders[column].fit_transform(result[column])
+    return result, encoders
+
+# Calculate the correlation and plot it
+encoded_data, _ = number_encode_features(original_data)
+sns.heatmap(encoded_data.corr(), square=True)
+plt.show()
+
+print(original_data[["Education", "Education-Num"]].head(15))
+
+del original_data["Education"]
+
+# zamiana stringow na wartosci liczbowe (klasyfikacja)
+encoded_data, encoders = number_encode_features(original_data)
+fig = plt.figure(figsize=(20,15))
+cols = 5
+rows = ceil(float(encoded_data.shape[1]) / cols)
+for i, column in enumerate(encoded_data.columns):
+    ax = fig.add_subplot(rows, cols, i + 1)
+    ax.set_title(column)
+    encoded_data[column].hist(axes=ax)
+    plt.xticks(rotation="vertical")
 plt.subplots_adjust(hspace=0.7, wspace=0.2)
 plt.show()
